b54e691307087a92cbe1d97af54785762e35cfc0
Fuzss <28218241+Fuzss@users.noreply.github.com>
2022-03-03 19:32:45 +0100

    1.18.2 update

--- src/main/java/fuzs/puzzleslib/PuzzlesLib.java
+++ src/main/java/fuzs/puzzleslib/PuzzlesLib.java
@@ -1,20 +1,23 @@
 package fuzs.puzzleslib;
 
-import net.fabricmc.api.ModInitializer;
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
  * utility class for common helper methods
  * also main puzzles lib mod, only really need, so it shows in the mods list
  */
-public class PuzzlesLib implements ModInitializer {
+public class PuzzlesLib {
+    /**
+     * puzzles lib mod id
+     */
     public static final String MOD_ID = "puzzleslib";
+    /**
+     * puzzles lib mod name
+     */
     public static final String MOD_NAME = "Puzzles Lib";
-    public static final Logger LOGGER = LogManager.getLogger(PuzzlesLib.MOD_NAME);
-
-    @Override
-    public void onInitialize() {
-
-    }
+    /**
+     * puzzles lib logger
+     */
+    public static final Logger LOGGER = LoggerFactory.getLogger(MOD_NAME);
 }
